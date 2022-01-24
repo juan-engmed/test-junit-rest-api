dfe4876636d53637625546ad61d63201a4eda1ec
Juan Oliveira <jfs.dev.eng@gmail.com>
2022-01-24 19:02:56 -0300

    first upload

---
+++ src/main/java/com/example/practice_test_api/PracticeTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_test_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeTestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_test_api/PracticeTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_test_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
