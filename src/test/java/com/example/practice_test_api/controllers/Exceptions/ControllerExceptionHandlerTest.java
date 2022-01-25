4da5f700e274952c34ca3006d1253fd1801c5a82
Juan Oliveira <jfs.dev.eng@gmail.com>
2022-01-25 14:28:37 -0300

    upload objectNotFound test

---
+++ src/test/java/com/example/practice_test_api/controllers/Exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,47 @@
+package com.example.practice_test_api.controllers.Exceptions;
+
+import com.example.practice_test_api.services.Exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+@SpringBootTest
+class ControllerExceptionHandlerTest {
+
+    @InjectMocks
+    private ControllerExceptionHandler controllerExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+    }
+
+    @Test
+    void objectNotFound() {
+        var response = controllerExceptionHandler
+                .objectNotFound(
+                        new ObjectNotFoundException("Objeto não encontrad"),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(404, response.getBody().getStatus());
+
+    }
+
+    @Test
+    void dataIntegrityViolation() {
+    }
+}
