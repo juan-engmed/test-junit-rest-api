36236355e3b39107fdd07ad46676131bf5e299df
Juan Oliveira <jfs.dev.eng@gmail.com>
2022-01-25 14:22:19 -0300

    upload controlle delete final test

--- src/test/java/com/example/practice_test_api/controllers/UserControllerTest.java
+++ src/test/java/com/example/practice_test_api/controllers/UserControllerTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserControllerTest {
@@ -114,11 +114,20 @@ class UserControllerTest {
         assertEquals(NAME, response.getBody().getName());
         assertEquals(EMAIL, response.getBody().getEmail());
 
-
     }
 
     @Test
     void delete() {
+        doNothing().when(userServiceImpl).delete(anyInt());
+
+        ResponseEntity<UserDto> response = userController.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+        verify(userServiceImpl, times(1)).delete(anyInt());
+
     }
 
     private void startUser(){
